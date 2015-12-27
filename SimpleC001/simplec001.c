1749fa5f5c2af57a2a3813c1fba404301efa20fd
shouzo <danny200026@yahoo.com.tw>
2015-12-27 22:41:25 +0800

    20151227 初探Buffer Overflow

---
+++ SimpleC001/simplec001.c
@@ -0,0 +1,20 @@
+// simplec001.c
+void func(char *str) {
+     char buffer[24];
+     int *ret;
+     strcpy(buffer, str);
+}
+
+
+int main(int argc, char **argv) {
+    int x;
+    x = 0;
+    func(argv[1]);
+    x = 1;
+    
+    printf("x is 1\n");
+    printf("x is 0\n");
+    system("pause");
+}
+    
+     
